e1131abf926ec71f7ef590fc69fcf51a50f0a045
0x-70da <mahmoudAnonymous0@gmail.com>
2026-02-09 10:44:36 +0200

    setup done

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router ,Router as ExpressRouter } from "express";
+// import authRoutes from "./features/auth/auth.routes";
+// import productRoutes from "./features/products/products.routes";
+
+const router: ExpressRouter = Router();
+
+// router.use("/auth", authRoutes);
+// router.use("/products", productRoutes);
+
+export default router;
