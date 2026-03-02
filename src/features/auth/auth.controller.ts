e67d5bed8dfe7c869d36cd3edbe092b31362177e
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-02 17:23:59 +0200

    Refactor authentication controller to use Supabase user management and improve request typing

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,47 +1,54 @@
 import type { Request, Response } from "express";
 import { supabase } from "../../config/supabase.js";
+import type { User } from "../users/users.types.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import type { LoginRequest, RegisterRequest } from "./auth.types.js";
 dotenv.config();
 
-export const register = async (req: Request, res: Response) => {
-    const {firstName, lastName, email, password} = req.body;
+export const register = async (req: RegisterRequest, res: Response) => {
+    const { firstName, lastName, email, password } = req.body;
 
-    const { data, error } = await supabase.auth.signUp({
-        email,
-        password,
-    });
+    const { data, error } = await supabase.from("users").select("*").eq("email", email);
 
-    if(error || !data.user){
-        return res.status(400).json({message: error?.message});
+    if(error) {
+        return res.status(500).json({success: false, message: `Internal Server Error`});
     }
 
+    if(data && data.length > 0) {
+        return res.status(400).json({success: false, message: "User already exists"});
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+
     await supabase.from("users").insert({
-        id: data.user.id,
         email,
+        password: hashed,
         first_name: firstName,
         last_name: lastName,
     });
 
-    return res.status(201).json({message: "User registered successfully"});
+    return res.status(201).json({success: true, message: "User registered successfully"});
 }
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: LoginRequest, res: Response) => {
     const { email, password } = req.body;
 
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-    });
+    const { data, error } = await supabase.from("users").select("*").eq("email", email).single();
+    const user = data as User;
 
-    if(error) {
-        return res.status(401).json({message: "Invalid email or password"});
+    if(error || !user) {
+        return res.status(401).json({success: false, message: "Invalid email or password"});
     }
 
-    const token = jwt.sign({ id: data.user.id, role: data.user.role?? "USER" }, process.env.JWT_SECRET!, { expiresIn: "1h" });
+    const matched = await bcrypt.compare(password, user.password);
 
-    return res.json({
-        user: data.user,
-        token,
-    })
+    if(!matched) {
+        return res.status(401).json({success: false, message: "Invalid email or password"});
+    }
+
+    const token = jwt.sign({ id: user.id, role: user.role?? "USER" }, process.env.JWT_SECRET!, { expiresIn: "1h" });
+
+    return res.status(200).cookie("token", token, { httpOnly: true }).json({success: true, message: "Logged in successfully"});
 }
