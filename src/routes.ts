3721fa6d432a11bd50930d91bef37a17d6cb1058
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-03 13:59:59 +0200

    Enhance cart and product types, and refactor auth middleware and routes for improved structure and clarity

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,18 @@
 import { Router ,Router as ExpressRouter } from "express";
+import productsRouter from "./features/products/products.routes.js";
+import authRouter from "./features/auth/auth.routes.js";
+import usersRouter from "./features/users/users.routes.js";
+import cartRouter from "./features/cart/cart.routes.js";
+import wishlistRouter from "./features/wishlist/wishlist.routes.js";
+import categoryRouter from "./features/category/category.routes.js";
 
 const router: ExpressRouter = Router();
 
-router.use("/products", (await import("./features/products/products.routes.js")).default);
-router.use("/auth", (await import("./features/auth/auth.routes.js")).default);
-router.use("/users", (await import("./features/users/users.routes.js")).default);
-router.use("/cart", (await import("./features/cart/cart.routes.js")).default);
-router.use("/wishlist", (await import("./features/wishlist/wishlist.routes.js")).default);
-router.use("/category", (await import("./features/category/category.routes.js")).default);
+router.use("/products", productsRouter);
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+router.use("/cart", cartRouter);
+router.use("/wishlist", wishlistRouter);
+router.use("/category", categoryRouter);
 
 export default router;
