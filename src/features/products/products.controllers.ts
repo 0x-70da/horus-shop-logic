5d5608af286c41086eb7f9936b03252f2a010a46
Mahmoud AbdElnasser <mahmoudAnonymous0@gmail.com>
2026-03-14 00:40:40 +0200

    Fix product not found check

--- src/features/products/products.controllers.ts
+++ src/features/products/products.controllers.ts
@@ -21,14 +21,15 @@ export const getProductBySlug = async (req: Request<{ slug: string }>, res: Resp
     .eq("slug", slug)
     .single();
 
+    if(!product) {
+        return res.status(404).json({success: false, message: "Product Not Found" });
+    }
+    
     if(error) {
         console.log("Error fetching product by slug:", error);
         return res.status(500).json({success: false, message: "Failed to fetch product"});
     }
 
-    if(!product) {
-        return res.status(404).json({success: false, message: "Product Not Found" });
-    }
 
     return res.status(200).json({success: true, data: product });
 }
