9b9fb0133f8b320d2c57778d93712e3e936f9625
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-13 23:31:44 +0200

    Refactor product controller responses to include success status and improve error handling

--- src/features/products/products.controllers.ts
+++ src/features/products/products.controllers.ts
@@ -3,27 +3,51 @@ import { supabase } from "../../config/supabase.js";
 
 export const getAllProducts = async (req: Request, res: Response) => {
 
-    const { data, error } = await supabase.from("products").select("*");
+    const { data: products, error } = await supabase.from("products").select("*");
 
     if(error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({success: false, message: "Failed to fetch products: " + error.message });
     }
 
-    return res.status(200).json(data);
+    return res.status(200).json({success: true, data: products });
 }
 
-export const getProductBySlug = async (req: Request, res: Response) => {
+export const getProductBySlug = async (req: Request<{ slug: string }>, res: Response) => {
     const { slug } = req.params;
 
-    const { data, error } = await supabase
+    const { data: product, error } = await supabase
     .from("products")
     .select("*")
     .eq("slug", slug)
     .single();
 
     if(error) {
-        return res.status(404).json({ message: "Product Not Found" });
+        console.log("Error fetching product by slug:", error);
+        return res.status(500).json({success: false, message: "Failed to fetch product"});
     }
 
-    return res.status(200).json(data);
+    if(!product) {
+        return res.status(404).json({success: false, message: "Product Not Found" });
+    }
+
+    return res.status(200).json({success: true, data: product });
+}
+
+export const getProductsByCategory = async (req: Request<{slug: string}>, res: Response) => {
+    const { slug } = req.params;
+
+    const { data: products, error } = await supabase
+    .from("products")
+    .select("*")
+    .eq("category_slug", slug);
+
+    if (error) {
+        return res.status(500).json({success: false, message: "Failed to fetch products" });
+    }
+
+    if (!products || products.length === 0) {
+        return res.status(404).json({success: false, message: "No products found for this category" });
+    }
+
+    res.status(200).json({success: true, data: products });
 }
