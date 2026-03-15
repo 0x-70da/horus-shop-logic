6466c826a3cd1e4438e4c7a7d6ff9d358f3b6265
Mahmoud AbdElnasser <mahmoudAnonymous0@gmail.com>
2026-03-15 14:43:19 +0200

    Add route for getting products by category

--- src/features/products/products.routes.ts
+++ src/features/products/products.routes.ts
@@ -1,9 +1,10 @@
 import { Router, Router as ExpressRouter } from "express";
-import { getAllProducts, getProductBySlug } from "./products.controller.js";
+import { getAllProducts, getProductBySlug, getProductByCategory } from "./products.controller.js";
 
 const router: ExpressRouter = Router();
 
 router.get("/", getAllProducts);
 router.get("/:slug", getProductBySlug);
+router.get("/:slug", getProductByCategory);
 
 export default router;
