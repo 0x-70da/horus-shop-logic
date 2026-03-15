eedc3c60838fb6d61edd3109e70f52d825bb48e6
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-15 13:55:30 +0200

    Refactor category feature to improve error handling and remove unused route

--- src/features/category/category.controller.ts
+++ src/features/category/category.controller.ts
@@ -2,16 +2,21 @@ import type { Request, Response } from "express";
 import { supabase } from "../../config/supabase.js";
 
 export const getAllCategories = async (req: Request, res: Response) => {
-    const { data: categories, error } = await supabase.from("categories").select("*");
-
-    if (error) {
-        console.log("Error fetching categories:", error);
-        return res.status(400).json({success: false, message: "Failed to fetch categories"});
+    try {
+        const { data: categories, error } = await supabase.from("categories").select("*");
+    
+        if (error) {
+            console.log("Error fetching categories:", error);
+            return res.status(400).json({success: false, message: "Failed to fetch categories"});
+        }
+    
+        if(!categories || categories.length === 0) {
+            return res.status(404).json({success: false, message: "No categories found" });
+        }
+    
+        res.status(200).json({success: true, data: categories });
+        
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error" });
     }
-
-    if(!categories || categories.length === 0) {
-        return res.status(404).json({success: false, message: "No categories found" });
-    }
-
-    res.status(200).json({success: true, data: categories });
 }
