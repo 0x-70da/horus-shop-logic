50d2544d47b772a6854cdce6287f2002332f0e11
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-23 23:00:03 +0200

    Add refresh token handling and JWT type definitions

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,15 +1,28 @@
 import jwt, { type JwtPayload, type SignOptions } from "jsonwebtoken";
 import dotenv from "dotenv";
+import type { MyJwtPayload } from "../types/jwt.types.js";
 dotenv.config();
 
-export const generateToken = (payload: JwtPayload, expiresIn: SignOptions["expiresIn"] = "1h"): string => {
-    return jwt.sign(payload, process.env.JWT_SECRET! as string, { expiresIn });
+export const generateAccessToken = (payload: JwtPayload, expiresIn: SignOptions["expiresIn"] = "15m"): string => {
+    return jwt.sign(payload, process.env.JWT_ACCESS_SECRET! as string, { expiresIn });
 }
 
-export const verifyToken = (token: string): JwtPayload | string => {
-    try {
-        return jwt.verify(token, process.env.JWT_SECRET!);
-    } catch (error) {
-        throw new Error("Invalid token");
+export const generateRefreshToken = (payload: JwtPayload, expiresIn: SignOptions["expiresIn"] = "7d"): string => {
+    return jwt.sign(payload, process.env.JWT_REFRESH_SECRET! as string, { expiresIn });
+}
+
+export const verifyAccessToken = (token: string): MyJwtPayload => {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!);
+    if (typeof decoded === "string") {
+        throw new Error("Invalid access token payload");
     }
+    return decoded as MyJwtPayload;
+}
+
+export const verifyRefreshToken = (token: string): MyJwtPayload => {
+    const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET!);
+    if (typeof decoded === "string") {
+        throw new Error("Invalid refresh token payload");
+    }
+    return decoded as MyJwtPayload;
 }
