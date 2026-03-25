0b954a3844992feed64ad8f7c4cc781fba879d6d
0x70da <mahmoudAnonymous0@gmail.com>
2026-03-25 22:06:25 +0200

    Add refresh token management and bcrypt utility functions with improved error logging

--- src/types/jwt.types.ts
+++ src/types/jwt.types.ts
@@ -1,6 +1,6 @@
 import type { JwtPayload } from "jsonwebtoken";
 
-export interface MyJwtPayload extends JwtPayload {
+export interface AuthJwtPayload extends JwtPayload {
     id: string;
     role: string;
 }
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt, { type JwtPayload, type SignOptions } from "jsonwebtoken";
 import dotenv from "dotenv";
-import type { MyJwtPayload } from "../types/jwt.types.js";
+import type { AuthJwtPayload } from "../types/jwt.types.js";
 dotenv.config();
 
 export const generateAccessToken = (payload: JwtPayload, expiresIn: SignOptions["expiresIn"] = "15m"): string => {
@@ -11,18 +11,18 @@ export const generateRefreshToken = (payload: JwtPayload, expiresIn: SignOptions
     return jwt.sign(payload, process.env.JWT_REFRESH_SECRET! as string, { expiresIn });
 }
 
-export const verifyAccessToken = (token: string): MyJwtPayload => {
+export const verifyAccessToken = (token: string): AuthJwtPayload => {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET!);
     if (typeof decoded === "string") {
         throw new Error("Invalid access token payload");
     }
-    return decoded as MyJwtPayload;
+    return decoded as AuthJwtPayload;
 }
 
-export const verifyRefreshToken = (token: string): MyJwtPayload => {
+export const verifyRefreshToken = (token: string): AuthJwtPayload => {
     const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET!);
     if (typeof decoded === "string") {
         throw new Error("Invalid refresh token payload");
     }
-    return decoded as MyJwtPayload;
+    return decoded as AuthJwtPayload;
 }
