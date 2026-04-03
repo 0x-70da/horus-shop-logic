9beca685b5ae52425a42bec5558c06984ade7cb6
0x70da <mahmoudAnonymous0@gmail.com>
2026-04-03 14:50:25 +0200

    Enhance GetProductsQuery by adding brand, minPrice, maxPrice, and inStock fields

--- src/features/products/products.types.ts
+++ src/features/products/products.types.ts
@@ -1,3 +1,5 @@
+import type { ReplaceKey } from "../../types/general.types.js";
+
 export type SortBy = "price" | "newest" | "popularity" | "rating";
 
 export type SortOrder = "asc" | "desc";
@@ -5,8 +7,14 @@ export type SortOrder = "asc" | "desc";
 export interface GetProductsQuery {
     category?: string;
     subcategory?: string;
+    brand?: string;
+    minPrice?: string;
+    maxPrice?: string;
+    inStock?: string;
     sortBy?: SortBy;
     sortOrder?: SortOrder;
     page?: string;
     limit?: string;
 }
+
+export type ValidatedGetProductsQuery = ReplaceKey<GetProductsQuery, 'page' | 'limit', number>;
